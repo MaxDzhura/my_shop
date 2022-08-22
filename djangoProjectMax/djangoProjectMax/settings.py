1d5386546471558e6256b341ef24fd7e73993b01
MaxDzhura <danik151214>
2022-08-22 13:28:28 +0300

    fix settings

--- djangoProjectMax/djangoProjectMax/settings.py
+++ djangoProjectMax/djangoProjectMax/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g%5+b_7o2!a8e2)36+2%miu)*&dq&zyy5(^uul5stk*k&u)d#r'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
@@ -33,7 +34,7 @@ DB_PORT = os.environ.get('DB_PORT') or '5432'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['restaurant-max.herokuapp.com', ]
 
 
 # Application definition
