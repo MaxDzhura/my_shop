3428a4d4e6adb5ea4ca0e19c2c94cb9565428980
MaxDzhura <105851617+MaxDzhura@users.noreply.github.com>
2022-09-28 12:28:23 +0000

    Update settings.py

--- my_shop/my_shop/settings.py
+++ my_shop/my_shop/settings.py
@@ -32,7 +32,7 @@ DB_PORT = os.environ.get('DB_PORT') or '5432'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['my---shop.herokuapp.com',]
 
