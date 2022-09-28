f71e43a0f641b48219b8b0305035139cab279eb0
MaxDzhura <105851617+MaxDzhura@users.noreply.github.com>
2022-09-28 11:09:19 +0000

    Delete Myshop directory

--- Myshop/landing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include
-from django.contrib import admin
-from landing import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('landing/', views.landing, name='landing'),
-]
--- Myshop/orders/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import *
-
-
-class CheckoutContactForm(forms.Form):
-    name = forms.CharField(required=True)
-    phone = forms.CharField(required=True)
